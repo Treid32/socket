6599e4f537f12826f4f03254e9e0218ac27941de
Treid32 <71757723+Treid32@users.noreply.github.com>
2021-02-10 10:00:19 +0100

    Aggiunte funzioni server

--- Server.py
+++ Server.py
@@ -5,67 +5,74 @@ import socket
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
 
-sock_listen = socket.socket()
-sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
-sock_listen.listen(5)
-print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT)))
-
-while True:
-    #Connessione col Client
-    sock_service, addr_client = sock_listen.accept()
-    print("\nConnessione ricevuta da " + str(addr_client))
-    print("\nAspetto di ricevere i dati ")
-    contConn = 0
-
-    #Ricezione dati
+#Funzione per verificare i dati
+def riceviComandi(socket):
     while True:
-        dati = sock_service.recv(2048)
-        contConn += 1
-        if not dati:
-            print("Fine dati dal client. Reset")
-            break
-        
-        #Decodifica dati
-        dati = dati.decode()
-        print("Ricevuto: '%s'" % dati)
-        if dati == '0':
-            print("Chiudo la connessione con " + str(addr_client))
-            break
+        sock_service, addr_client = socket.accept()
+        print("\nConnessione ricevuta da " + str(addr_client))          #Connessione col Client
+        print("\nAspetto di ricevere i dati ")
+        contConn = 0
+        while True:
+            dati = sock_service.recv(2048)
+            contConn += 1
+            if not dati:
+                print("Fine dati dal client. Reset")
+                break
 
-        dati = dati.split(";")  # operatore;primo numero;secondo numero -> [piu][primo numero][secondo numero]
-        risposta = str()
+            dati = dati.decode()
+            print("Ricevuto: '%s'" % dati)                              #Ricezione e decodifica dati
+            if dati == '0':
+                print("Chiudo la connessione con " + str(addr_client))
+                break
 
-        #Verifica dell'operatore e successivamente dei numeri
-        if dati[0] == "piu" or dati[0] == "meno" or dati[0] == "per" or dati[0] == "diviso":
-            try:
-                dati[1] = int(dati[1])
-                dati[2] = int(dati[2])
-            except ValueError:
-                print("ValueError")
-                risposta = "Non hai inserito i numeri correttamente."
+            dati = dati.split(";")  # operatore;primo numero;secondo numero -> [piu][primo numero][secondo numero]
+            risposta = str()
 
-            if risposta == "":  #Valori corretti
-                risultato = int()
+            #Verifica dell'operatore e successivamente dei numeri
+            if dati[0] == "piu" or dati[0] == "meno" or dati[0] == "per" or dati[0] == "diviso":
+                try:
+                    dati[1] = int(dati[1])
+                    dati[2] = int(dati[2])
+                except ValueError:
+                    print("ValueError")
+                    risposta = "Non hai inserito i numeri correttamente."
 
-                #Calcolo dell'operazione
-                if dati[0] == "piu":
-                    risultato = dati[1] + dati[2]
-                elif dati[0] == "meno":
-                    risultato = dati[1] - dati[2]
-                elif dati[0] == "per":
-                    risultato = dati[1] * dati[2]
-                else:
-                    risultato = dati[1] / dati[2]
+                if risposta == "":  #Valori corretti
+                    risultato = int()
+ 
+                    #Calcolo dell'operazione
+                    if dati[0] == "piu":
+                        risultato = dati[1] + dati[2]
+                    elif dati[0] == "meno":
+                        risultato = dati[1] - dati[2]
+                    elif dati[0] == "per":
+                        risultato = dati[1] * dati[2]
+                    else:
+                        risultato = dati[1] / dati[2]
 
-                #Costruzione stringa
-                risposta = "Il risultato dell'operazione " + \
-                    str(dati[0]) + " tra " + str(dati[1]) + " e " + \
-                    str(dati[2]) + " è uguale a " + str(risultato) + "."
-        else:
-            risposta = "Operazione non valida."
+                    
+                    #Costruzione stringa
+                    risposta = "Il risultato dell'operazione " + \
+                        str(dati[0]) + " tra " + str(dati[1]) + " e " + \
+                        str(dati[2]) + " è uguale a " + str(risultato) + "."
+            else:
+                risposta = "Operazione non valida."
 
-        risposta = risposta.encode()
-        sock_service.send(risposta)
+            risposta = risposta.encode()
 
-    sock_service.close()
+            sock_service.send(risposta)
+        sock_service.close()
+
+#Funzione di avvio Server
+def avviaServer(address, port):
+    sock_listen = socket.socket()
+    sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock_listen.bind((address, port))
+    sock_listen.listen(5)
+    print("Server in ascolto su %s." % str((address, port)))
+
+    riceviComandi(sock_listen)
+
+
+if __name__ == "__main__":
+    avviaServer(SERVER_ADDRESS, SERVER_PORT)
